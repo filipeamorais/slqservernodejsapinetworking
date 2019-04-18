d542e8ff71b8ac232733af2f0ed4b399e5214969
Filipe de Andrade Morais <afilipem@gmail.com>
2019-04-18 00:18:05 -0500

    new: adding the routes for the functions that are expected for the Bookstore

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -3,34 +3,171 @@ const express = require ('express')
 const appRouter = express ()
 const bodyParser = require ('body-parser')
 appRouter.use(bodyParser.json())
+appRouter.use(express.static('public'));
+
 
 //HTTP methods in routing fashion
 appRouter.route('/')
 .all((req,res,next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/http');
+    res.setHeader('Content-Type', 'text/html');
     next();
   })
 .get((req,res,next) => {
-    day = req.query.day
-    time = req.query.time
-    title = req.query.title
-    res.write('<H1>Editing Appointment: ' + day + ' / ' + time + ' / ' + title+ '</H1>')
-    res.end('\n<h2>Sending appointment, ID = ' + req.params.id + '</h2>')
+    res.sendFile( __dirname + "/" + "index.html" );
 })
-.post((req, res, next) => {
-    day = req.body.day
-    time = req.body.time
-    title = req.body.title
-    res.write('<H1>Editing Appointment: ' + day + ' / ' + time + ' / ' + title+ '</H1>')
-    res.end('\n<h2>Adding appointment, ID = ' + req.params.id + '</h2>')
+// .post((req, res, next) => {
+//     day = req.body.day
+//     time = req.body.time
+//     title = req.body.title
+//     res.write('<H1>Editing Appointment: ' + day + ' / ' + time + ' / ' + title+ '</H1>')
+//     res.end('\n<h2>Adding appointment, ID = ' + req.params.id + '</h2>')
+// })
+// .put((req, res, next) => {
+//     res.statusCode = 403;
+//     res.end('Operation Not Supported.');
+// })
+// .delete((req, res, next) => {
+//     res.end('Deleting Appointment');
+// })
+
+appRouter.route('/add_page')
+.get((req,res,next) => {
+    res.sendFile("add_page.html", { root: "./public" } );
 })
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('Operation Not Supported.');
+
+appRouter.route('/adding')
+.get((req,res,next) => {
+    
+    
 })
-.delete((req, res, next) => {
-    res.end('Deleting Appointment');
+
+appRouter.route('/delete_page')
+.get((req,res,next) => {
+    res.sendFile("delete_page.html", { root: "./public" } );
+})
+
+appRouter.route('/find_page')
+.get((req,res,next) => {
+    res.sendFile("find_page.html", { root: "./public" } );
+})
+
+appRouter.route('/list_page')
+.get((req,res,next) => {
+    res.sendFile("list_page.html", { root: "./public" } );
+})
+
+appRouter.route('/modify_page')
+.get((req,res,next) => {
+    res.sendFile("modify_page.html", { root: "./public" } );
 })
 
 module.exports = appRouter
+
+function selectQuery() {
+    console.log('Reading rows from the Table...');
+    var request = new Request(
+        "SELECT * FROM Persons",
+        function(err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
+
+function addQuery() {
+    console.log('Reading rows from the Table...');
+    var request = new Request(
+        "SELECT * FROM Persons",
+        function(err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
+
+function deleteQuery() {
+    console.log('Reading rows from the Table...');
+    var request = new Request(
+        "SELECT * FROM Persons",
+        function(err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
+
+function findQuery() {
+    console.log('Reading rows from the Table...');
+    var request = new Request(
+        "SELECT * FROM Persons",
+        function(err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
+
+function listAllQuery() {
+    console.log('Reading rows from the Table...');
+    var request = new Request(
+        "SELECT * FROM Persons",
+        function(err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
+
+function modifyQuery() {
+    console.log('Reading rows from the Table...');
+    var request = new Request(
+        "SELECT * FROM Persons",
+        function(err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+            process.exit();
+        }
+    );
+
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+    });
+    connection.execSql(request);
+}
